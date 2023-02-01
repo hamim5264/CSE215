a9b093b8dcbe78fbfd7b857b348919237200a88e
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 20:18:35 +0600

    Create Demo1.java

---
+++ Demo/src/demo/Demo1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package demo;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+     int a = 10, b= 15, x= a+b;
+     System.out.println("Sum ="+x);
+    }
+    
+}
